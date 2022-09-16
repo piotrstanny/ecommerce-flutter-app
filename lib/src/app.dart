730a919af8be3056f9a7b6b0cbca340de11acc13
Piotr Stanny <piotr.stanny@gmail.com>
2022-09-16 19:37:13 +0100

    Change project structure to feature-first

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,6 @@
-import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,30 +1,30 @@
-import 'package:ecommerce_app/src/features/account/account_screen.dart';
-import 'package:ecommerce_app/src/features/not_found/not_found_screen.dart';
-import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
-import 'package:ecommerce_app/src/features/product_page/product_screen.dart';
-import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
-import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_screen.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_state.dart';
+import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_screen.dart';
+import 'package:ecommerce_app/src/features/checkout/presentation/checkout_screen/checkout_screen.dart';
+import 'package:ecommerce_app/src/features/orders/presentation/orders_list/orders_list_screen.dart';
+import 'package:ecommerce_app/src/features/products/presentation/product_screen/product_screen.dart';
+import 'package:ecommerce_app/src/features/products/presentation/products_list/products_list_screen.dart';
+import 'package:ecommerce_app/src/features/reviews/presentation/leave_review_screen/leave_review_screen.dart';
+import 'package:ecommerce_app/src/routing/not_found_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../features/checkout/checkout_screen.dart';
-import '../features/leave_review_page/leave_review_screen.dart';
-import '../features/products_list/products_list_screen.dart';
-import '../features/shopping_cart/shopping_cart_screen.dart';
 
 enum AppRoute {
   home,
+  product,
+  leaveReview,
   cart,
+  checkout,
   orders,
   account,
   signIn,
-  product,
-  leaveReview,
-  checkout,
 }
 
 final goRouter = GoRouter(
-  debugLogDiagnostics: false,
   initialLocation: '/',
+  debugLogDiagnostics: false,
   routes: [
     GoRoute(
       path: '/',
@@ -32,24 +32,47 @@ final goRouter = GoRouter(
       builder: (context, state) => const ProductsListScreen(),
       routes: [
         GoRoute(
-            path: 'cart',
-            name: AppRoute.cart.name,
-            pageBuilder: (context, state) => MaterialPage(
+          path: 'product/:id',
+          name: AppRoute.product.name,
+          builder: (context, state) {
+            final productId = state.params['id']!;
+            return ProductScreen(productId: productId);
+          },
+          routes: [
+            GoRoute(
+              path: 'review',
+              name: AppRoute.leaveReview.name,
+              pageBuilder: (context, state) {
+                final productId = state.params['id']!;
+                return MaterialPage(
                   key: state.pageKey,
                   fullscreenDialog: true,
-                  child: const ShoppingCartScreen(),
-                ),
-            routes: [
-              GoRoute(
-                path: 'checkout',
-                name: AppRoute.checkout.name,
-                pageBuilder: (context, state) => MaterialPage(
-                  key: state.pageKey,
-                  fullscreenDialog: true,
-                  child: const CheckoutScreen(),
-                ),
+                  child: LeaveReviewScreen(productId: productId),
+                );
+              },
+            ),
+          ],
+        ),
+        GoRoute(
+          path: 'cart',
+          name: AppRoute.cart.name,
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const ShoppingCartScreen(),
+          ),
+          routes: [
+            GoRoute(
+              path: 'checkout',
+              name: AppRoute.checkout.name,
+              pageBuilder: (context, state) => MaterialPage(
+                key: state.pageKey,
+                fullscreenDialog: true,
+                child: const CheckoutScreen(),
               ),
-            ]),
+            ),
+          ],
+        ),
         GoRoute(
           path: 'orders',
           name: AppRoute.orders.name,
@@ -79,27 +102,6 @@ final goRouter = GoRouter(
             ),
           ),
         ),
-        GoRoute(
-            path: 'product/:id',
-            name: AppRoute.product.name,
-            builder: (context, state) {
-              final productId = state.params['id']!;
-              return ProductScreen(productId: productId);
-            },
-            routes: [
-              GoRoute(
-                path: 'review',
-                name: AppRoute.leaveReview.name,
-                pageBuilder: (context, state) {
-                  final productId = state.params['id']!;
-                  return MaterialPage(
-                    key: state.pageKey,
-                    fullscreenDialog: true,
-                    child: LeaveReviewScreen(productId: productId),
-                  );
-                },
-              ),
-            ]),
       ],
     ),
   ],
