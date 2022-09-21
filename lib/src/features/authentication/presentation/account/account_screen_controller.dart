f7e9e832936d39dfd846a0171b5386c25a507da7
Piotr Stanny <piotr.stanny@gmail.com>
2022-09-21 13:20:55 +0100

    Refactor by using guard() method; Extract error dialog for reusability

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -8,19 +8,18 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
   final AuthRepository authRepository;
 
   Future<bool> signOut() async {
-    // set state to laoding
-    // sign out using auth repository
-    // if succeed, set state to data
-    // if error, set state to error
-    try {
-      state = const AsyncValue<void>.loading();
-      await authRepository.signOut();
-      state = const AsyncValue<void>.data(null);
-      return true;
-    } catch (e) {
-      state = AsyncValue.error(e);
-      return false;
-    }
+    // try {
+    //   state = const AsyncValue<void>.loading();
+    //   await authRepository.signOut();
+    //   state = const AsyncValue<void>.data(null);
+    //   return true;
+    // } catch (e) {
+    //   state = AsyncValue.error(e);
+    //   return false;
+    // }
+    state = const AsyncValue<void>.loading();
+    state = await AsyncValue.guard(() => authRepository.signOut());
+    return state.hasError == false;
   }
 }
 
