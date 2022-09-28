6e6081e40870b3cc3c69728cf9f638c5ef858352
Piotr Stanny <piotr.stanny@gmail.com>
2022-09-28 20:32:58 +0100

    Finish testing sign in controller; Refactor test mock

--- test/src/features/authentication/presentation/sign_in/email_password_sign_in_controller_test.dart
+++ test/src/features/authentication/presentation/sign_in/email_password_sign_in_controller_test.dart
@@ -1,11 +1,10 @@
-import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_controller.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockAuthRepository extends Mock implements FakeAuthRepository {}
+import '../../../../mocks.dart';
 
 void main() {
   const testEmail = 'test@test.com';
@@ -156,7 +155,37 @@ void main() {
       final controller = EmailPasswordSignInController(
           authRepository: authRepository,
           formType: EmailPasswordSignInFormType.signIn);
+      // run
+      controller.updateFormType(EmailPasswordSignInFormType.register);
+      // verify
+      expect(
+        controller.debugState,
+        EmailPasswordSignInState(
+          formType: EmailPasswordSignInFormType.register,
+          value: const AsyncData<void>(null),
+        ),
+      );
     });
-    // run
+    test('''
+    Given formType is register
+    When called with signIn
+    Then state.formType is signIn
+    ''', () {
+      // setup
+      final authRepository = MockAuthRepository();
+      final controller = EmailPasswordSignInController(
+          authRepository: authRepository,
+          formType: EmailPasswordSignInFormType.register);
+      // run
+      controller.updateFormType(EmailPasswordSignInFormType.signIn);
+      // verify
+      expect(
+        controller.debugState,
+        EmailPasswordSignInState(
+          formType: EmailPasswordSignInFormType.signIn,
+          value: const AsyncData<void>(null),
+        ),
+      );
+    });
   });
 }
