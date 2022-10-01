bda161b7cd0c892eb0c47e45ba54cdd3bc332075
Piotr Stanny <piotr.stanny@gmail.com>
2022-10-01 18:51:17 +0100

    Use finding by widget keys in tests

--- test/src/features/authentication/auth_robot.dart
+++ test/src/features/authentication/auth_robot.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -19,6 +20,13 @@ class AuthRobot {
 
   Future<void> tapLogoutButton() async {
     final logoutButton = find.text('Logout');
+    expect(logoutButton, findsOneWidget);
+    await tester.tap(logoutButton);
+    await tester.pump();
+  }
+
+  Future<void> tapDialogLogoutButton() async {
+    final logoutButton = find.byKey(kDialogDefaultKey);
     expect(logoutButton, findsOneWidget);
     await tester.tap(logoutButton);
     await tester.pump();
