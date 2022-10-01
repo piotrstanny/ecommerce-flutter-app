74958e62be3355e16cdf01784112f0e46fd2ee0e
Piotr Stanny <piotr.stanny@gmail.com>
2022-10-01 18:28:20 +0100

    Implement Robot testing method

--- test/src/features/authentication/presentation/account/account_screen_test.dart
+++ test/src/features/authentication/presentation/account/account_screen_test.dart
@@ -1,16 +1,14 @@
-import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../auth_robot.dart';
 
 void main() {
   testWidgets('Cancel logout', (tester) async {
-    await tester.pumpWidget(
-      const ProviderScope(
-        child: MaterialApp(
-          home: AccountScreen(),
-        ),
-      ),
-    );
+    final r = AuthRobot(tester);
+    await r.pumpAccountScreen();
+    await r.tapLogoutButton();
+    r.expectLogoutDialogFound();
+    await r.tapCancelButton();
+    r.expectLogoutDialogNotFound();
   });
 }
