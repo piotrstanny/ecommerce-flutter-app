ccb2d0d9584de0d273ce4b4518523ec500e944bb
Piotr Stanny <piotr.stanny@gmail.com>
2022-10-04 10:59:11 +0100

    Complete sign in test for valid email and password input

--- test/src/features/authentication/presentation/sign_in/email_password_sign_in_screen_test.dart
+++ test/src/features/authentication/presentation/sign_in/email_password_sign_in_screen_test.dart
@@ -20,9 +20,38 @@ void main() {
       await r.pumpEmailPasswordSignInContents(
           authRepository: authRepository,
           formType: EmailPasswordSignInFormType.signIn);
-      r.tapEmailAndPasswordSubmitButton();
+      await r.tapEmailAndPasswordSubmitButton();
       verifyNever(
           () => authRepository.signInWithEmailAndPassword(any(), any()));
+    });
+
+    testWidgets('''
+    Given formType is signIn
+    When enter a valid email and password
+    Then signInWithEmailAndPassword is called
+    And onSignedIn callback is called
+    And error alert is not shown
+    ''', (tester) async {
+      final r = AuthRobot(tester);
+      var didSignIn = false;
+      when(() => authRepository.signInWithEmailAndPassword(
+            testEmail,
+            testPassword,
+          )).thenAnswer((_) => Future.value());
+      await r.pumpEmailPasswordSignInContents(
+        authRepository: authRepository,
+        formType: EmailPasswordSignInFormType.signIn,
+        onSignedIn: () => didSignIn = true,
+      );
+      await r.enterEmail(testEmail);
+      await r.enterPassword(testPassword);
+      await r.tapEmailAndPasswordSubmitButton();
+      verify(() => authRepository.signInWithEmailAndPassword(
+            testEmail,
+            testPassword,
+          )).called(1);
+      r.expectErrorAlertNotFound();
+      expect(didSignIn, true);
     });
   });
 }
